cf23269f21fbc75d525a407b57706beab0ccb769
shourya <shourya.sharma@mail.utoronto.ca>
2019-03-25 09:47:25 -0400

    Listener for account interface created

--- phase2/phase2/UserInterface.java
+++ phase2/phase2/UserInterface.java
@@ -1,6 +1,66 @@
 package phase2;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class UserInterface extends JFrame {
+    private JCheckBox changePasswordCheckBox;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton setPasswordButton;
+    private JButton makeTransactionButton;
+    private JButton logOutButton;
+
+    public UserInterface() {
+        changePasswordCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(changePasswordCheckBox.isSelected()){
+
+
+                }
+
+            }
+        });
+        passwordField2.addInputMethodListener(new InputMethodListener() {
+            @Override
+            public void inputMethodTextChanged(InputMethodEvent event) {
+
+            }
+
+            @Override
+            public void caretPositionChanged(InputMethodEvent event) {
+
+            }
+        });
+        passwordField1.addInputMethodListener(new InputMethodListener() {
+            @Override
+            public void inputMethodTextChanged(InputMethodEvent event) {
+
+            }
+
+            @Override
+            public void caretPositionChanged(InputMethodEvent event) {
+
+            }
+        });
+        setPasswordButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        makeTransactionButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        logOutButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 }
