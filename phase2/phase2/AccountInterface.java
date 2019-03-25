7c0625d37b3abb04bcec6983db8edcf2206db408
shourya <shourya.sharma@mail.utoronto.ca>
2019-03-25 08:55:27 -0400

    Listener for account interface created

--- phase2/phase2/AccountInterface.java
+++ phase2/phase2/AccountInterface.java
@@ -1,6 +1,41 @@
 package phase2;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AccountInterface extends JFrame {
+    private JButton clickHereToAddButton;
+    private JComboBox comboBox1;
+    private JButton exitButton;
+    private JButton makeTransactionButton;
+
+    public AccountInterface() {
+        clickHereToAddButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        makeTransactionButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        exitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 }
