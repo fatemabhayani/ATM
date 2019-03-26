a77c6845acfc8e9aa898201f370e5015f6df3122
brittapal <brittany.palandra@mail.utoronto.ca>
2019-03-26 17:06:18 -0400

    worked on writeUserData method

--- phase2/phase2/DataSaver.java
+++ phase2/phase2/DataSaver.java
@@ -2,6 +2,7 @@ package phase2;
 
 import java.io.*;
 import phase2.Display.ATM;
+import phase2.People.*;
 
 public class DataSaver {
 
@@ -51,6 +52,19 @@ public class DataSaver {
     private static void writeUserData() {
         try (FileWriter writer = new FileWriter(userdata)) {
              writer.write("ATM BANK USERS");
+             for (User u: ATM.bankUsers) {
+                 writer.write("USER");
+                 writer.write(u.toString());
+                 writer.write("SAVINGS");
+
+                 writer.write("CHEQUING");
+
+                 writer.write("CASH BACK");
+
+                 writer.write("CREDIT CARD");
+
+                 writer.write("LINE OF CREDIT");
+             }
         } catch (Exception e) {
             e.printStackTrace();
         }
