22dc8a8db02cf87a77c15f285ae52a23174b84f9
brittapal <brittany.palandra@mail.utoronto.ca>
2019-03-27 11:28:21 -0400

    javadoc for transactioncomparator and bill

--- phase2/phase2/Transactions/Bill.java
+++ phase2/phase2/Transactions/Bill.java
@@ -1,31 +1,33 @@
 package phase2.Transactions;
 
-import phase2.Accounts.Account;
-import phase2.Accounts.CreditCard;
-import phase2.Accounts.LineOfCredit;
-import phase2.Accounts.Savings;
+import phase2.Accounts.*;
 import phase2.ForeignCurrency;
-import phase2.Transactions.Transaction;
-
 import java.util.Calendar;
 
 public class Bill extends Transaction {
 
+    /**
+     * The account paying the bill.
+     */
     private Account moneyFrom;
 
     /**
-     * Instantiates a new Bill.
+     * Instantiates a new bill.
      *
      * @param amount    the amount payed
      * @param moneyFrom the account the money originates from
      * @param date      date of transaction
      */
-
     public Bill(ForeignCurrency amount, Account moneyFrom, Calendar date) {
         super(amount, date);
         this.moneyFrom = moneyFrom;
     }
 
+    /**
+     * Returns whether or not the bill payment is approved.
+     *
+     * @return true iff the transaction is approved
+     */
     public boolean transactionApproved() {
         if (moneyFrom.getClass().isInstance(CreditCard.class)) {
             return false;
@@ -35,18 +37,25 @@ public class Bill extends Transaction {
             return moneyFrom.getBalance().compareTo(getAmount()) > 0;
         } else {
             return moneyFrom.getBalance().compareTo(getAmount()) >= 0 &&
-                    moneyFrom.getBalance().compareTo
-                            (new ForeignCurrency("CAD", getAmount().
-                                    convert("CAD").getAmount() - 100)) >= 0;
+                    moneyFrom.getBalance().compareTo(new ForeignCurrency("CAD", getAmount().
+                            convert("CAD").getAmount() - 100)) >= 0;
         }
     }
 
+    /**
+     * Makes the transaction.
+     */
     public void makeTransaction() {
+        // Subtracts amount from moneyFrom balance and writes to outgoing.txt
         moneyFrom.subtract(amount);
-        // writes to outgoing.txt
     }
 
-    public void undoTransaction() {}
+    /**
+     * Undoes the transaction. Bills can't be undone, so this does nothing.
+     */
+    public void undoTransaction() {
+        System.out.println("Bill payments cannot be undone!");
+    }
 
     @Override
     public String toString() {
--- phase2/phase2/Transactions/TransactionComparator.java
+++ phase2/phase2/Transactions/TransactionComparator.java
@@ -5,6 +5,14 @@ import java.util.Comparator;
 
 public class TransactionComparator implements Comparator<Transaction> {
 
+    /**
+     * Compares the date of two transactions.
+     *
+     * @param t1 the first transaction
+     * @param t2 the second transaction
+     * @return a negative value if t1 was made earlier, a positive value if t1 was made later,
+     * and 0 if t1 and t2 were made at the same date
+     */
     public int compare(Transaction t1, Transaction t2) {
         Calendar time1 = t1.getTimeOfTransaction();
         Calendar time2 = t2.getTimeOfTransaction();
