a8c04d12c2db570b3c9386017244b0ac13edfffa
Sahni-Arjun <arjun.sahni@mail.utoronto.ca>
2019-03-29 01:25:39 -0400

    works for everything but request

--- phase2/phase2/Data/DataSaver.java
+++ phase2/phase2/Data/DataSaver.java
@@ -1,9 +1,13 @@
 package phase2.Data;
 
 import java.io.*;
+import java.util.GregorianCalendar;
+
 import phase2.Display.ATM;
+import phase2.ForeignCurrency;
 import phase2.People.*;
 import phase2.Accounts.*;
+import phase2.Transactions.*;
 
 /**
  * The type Data saver.
@@ -12,11 +16,16 @@ public class DataSaver {
 
     public static void main(String[] args) {
 
-        ATM.bankUsers.add(new User("x", "x"));
+        User user = new User("yes", "yes");
         ATM.bankEmployees.add(new BankTeller("yes", "yes"));
+        Savings s = new Savings(new GregorianCalendar(), user, "CAD", 0);
+        user.getAccountManager().add(s);
+        Deposit b = new Deposit(new ForeignCurrency("CAD", 50), s, new GregorianCalendar());
+        user.makeTransaction(b);
 
         DataSaver d = new DataSaver();
-        d.writeData();
+        d.writeUserData(user);
+
 
     }
 
@@ -33,19 +42,6 @@ public class DataSaver {
      */
     private DataSaver() {}
 
-    /**
-     * Writes the data from the ATM.
-     */
-    public void writeData() {
-        try {
-            writeATMData();
-            writeUserData();
-            writeTransactionData();
-            writeEmployeeData();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Writes the time and cash machine contents into atmdata.txt.
@@ -63,115 +59,104 @@ public class DataSaver {
         }
     }
 
-    /**
-     * Writes the bankUsers contents into userdata.txt.
-     */
-    private void writeUserData() {
-        try (FileWriter writer = new FileWriter(userdata)) {
-             writer.write("ATM BANK USERS");
-             for (User u: ATM.bankUsers) {
-                 writer.write("USER");
-                 writer.write(u.toString());
-                 writer.write("SAVINGS");
-                 for (Account account : u.getAccountList("sv")) {
-                     Savings a = (Savings) account;
-                     writer.write(a.toString());
-                 }
-                 writer.write("CHEQUING");
-                 for (Object o : u.getAccountList("cq")) {
-                     Chequing a = (Chequing) o;
-                     writer.write(a.toString());
-                 }
-                 writer.write("CASH BACK");
-                 for (Object o : u.getAccountList("cb")) {
-                     CashBackCard a = (CashBackCard) o;
-                     writer.write(a.toString());
-                 }
-                 writer.write("CREDIT CARD");
-                 for (Object o : u.getAccountList("cc")) {
-                     CreditCard a = (CreditCard) o;
-                     writer.write(a.toString());
-                 }
-                 writer.write("LINE OF CREDIT");
-                 for (Object o : u.getAccountList("lc")) {
-                     LineOfCredit a = (LineOfCredit) o;
-                     writer.write(a.toString());
-                 }
-             }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void writeAllUsers(){
+        for(User user: ATM.bankUsers){
+            writeUserData(user);
         }
     }
 
-    /**
-     * Writes the transaction information into transactiondata.txt.
-     */
-    private void writeTransactionData() {
-        try (FileWriter writer = new FileWriter(transactiondata)) {
-            writer.write("ATM TRANSACTIONS");
-            for (User u: ATM.bankUsers) {
-                // We only write a user to the file if the user has made any transactions.
-                if (u.getAccountManager().getTransactions().size() > 0) {
-                    writer.write("USER");
-                    writer.write(u.toString());
-                    for (Object o : u.getAccountList("sv")) {
-                        Savings a = (Savings) o;
-                        // We only write an account to the file if there are any transactions made on it.
-                        if (a.getTransactions().size() > 0) {
-                            writer.write("ACCOUNT: " + a.accountNum);
-                            writer.write(a.transactionString());
-                        }
-                    }
-                    for (Object o : u.getAccountList("cq")) {
-                        Chequing a = (Chequing) o;
-                        if (a.getTransactions().size() > 0) {
-                            writer.write("ACCOUNT: " + a.accountNum);
-                            writer.write(a.transactionString());
-                        }
-                    }
-                    for (Object o : u.getAccountList("cb")) {
-                        CashBackCard a = (CashBackCard) o;
-                        if (a.getTransactions().size() > 0) {
-                            writer.write("ACCOUNT: " + a.accountNum);
-                            writer.write(a.transactionString());
-                        }
-                    }
-                    for (Object o : u.getAccountList("cc")) {
-                        CreditCard a = (CreditCard) o;
-                        if (a.getTransactions().size() > 0) {
-                            writer.write("ACCOUNT: " + a.accountNum);
-                            writer.write(a.transactionString());
-                        }
-                    }
-                    for (Object o : u.getAccountList("lc")) {
-                        LineOfCredit a = (LineOfCredit) o;
-                        if (a.getTransactions().size() > 0) {
-                            writer.write("ACCOUNT: " + a.accountNum);
-                            writer.write(a.transactionString());
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
+
+    private void writeUserData(User user){
+        appendName(user);
+        try (FileWriter writer = new FileWriter("phase2/phase2/Data/UserDataFiles/" + user.getUsername() + ".txt")){
+            writeName(writer, user);
+            writeAllSavings(writer, user);
+            writeAllChequing(writer, user);
+            writeAllLineOfCredit(writer, user);
+            writeAllCreditCard(writer, user);
+            writeAllCashBackCard(writer, user);
+        } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+    private void appendName(User user){
+        try (FileWriter writer = new FileWriter("phase2/phase2/Data/UserDataFiles/ListOfNames.txt", true)){
+            writer.write(user.getUsername() + "\n");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void writeName(FileWriter writer, User user) throws IOException{
+        writer.write(user.toString() +"\n");
+    }
+
+    private void writeSavingAccount(FileWriter writer, Savings s) throws IOException {
+        writer.write(s.toString() +" ");
+        writer.write(s.transactionString() + "\n");
+    }
+
+    private void writeAllSavings(FileWriter writer, User user) throws IOException{
+        writer.write("SAVINGS \n");
+        for(Account account: user.getAccountList("sv")){
+            Savings s = (Savings) account;
+            writeSavingAccount(writer, s);
         }
     }
 
-    /**
-     * Writes the employee information into employeedata.txt.
-     */
-    private void writeEmployeeData() {
-        try (FileWriter writer = new FileWriter(employeedata)) {
-            writer.write("ATM BANK MANAGER \n");
-            writer.write(ATM.b.toString() + "\n");
-            writer.write("ATM BANK TELLERS \n");
-            for (BankTeller b : ATM.bankEmployees) {
-                writer.write(b.getUsername() + "." + b.getPassword() + "\n");
-                writer.write(b.toString() + "\n");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void writeChequingAccount(FileWriter writer, Chequing c) throws IOException {
+        writer.write(c.toString() +" ");
+        writer.write(c.transactionString() + "\n");
+    }
+
+    private void writeAllChequing(FileWriter writer, User user) throws IOException{
+        writer.write("CHEQUING \n");
+        for(Account account: user.getAccountList("cq")){
+            Chequing c = (Chequing) account;
+            writeChequingAccount(writer, c);
         }
     }
+
+    private void writeCreditCardAccount(FileWriter writer, CreditCard c) throws IOException {
+        writer.write(c.toString() + " ");
+        writer.write(c.transactionString() + "\n");
+    }
+
+    private void writeAllCreditCard(FileWriter writer, User user) throws IOException{
+        writer.write("CREDIT CARD \n");
+        for(Account account: user.getAccountList("cc")){
+            CreditCard c = (CreditCard) account;
+            writeCreditCardAccount(writer, c);
+        }
+    }
+
+    private void writeLineOfCreditAccount(FileWriter writer, LineOfCredit c) throws IOException {
+        writer.write(c.toString() + " ");
+        writer.write(c.transactionString() + "\n");
+    }
+
+
+    private void writeAllLineOfCredit(FileWriter writer, User user) throws IOException{
+        writer.write("LINE OF CREDIT \n");
+        for(Account account: user.getAccountList("lc")){
+            LineOfCredit c = (LineOfCredit) account;
+            writeLineOfCreditAccount(writer, c);
+        }
+    }
+
+    private void writeCashBackCardAccount(FileWriter writer, CashBackCard c) throws IOException {
+        writer.write(c.toString() + " ");
+        writer.write(c.transactionString() + "\n");
+    }
+
+
+    private void writeAllCashBackCard(FileWriter writer, User user) throws IOException{
+        writer.write("CASH BACK CARD \n");
+        for(Account account: user.getAccountList("cb")){
+            CashBackCard c = (CashBackCard) account;
+            writeCashBackCardAccount(writer, c);
+        }
+    }
+
+
 
 }
