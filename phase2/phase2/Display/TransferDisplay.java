b5aa91b6dbb7ccc00ad232b9af40a7956807f645
Fatema <fatema.alibhayani@gmail.com>
2019-03-30 23:11:01 -0400

    Implemented tradable in display

--- phase2/phase2/Display/TransferDisplay.java
+++ phase2/phase2/Display/TransferDisplay.java
@@ -3,6 +3,7 @@ package phase2.Display;
 import phase2.Accounts.Account;
 import phase2.People.User;
 import phase2.People.UserManager;
+import phase2.Tradable.ForeignCurrency;
 import phase2.Transactions.Transfer;
 
 import java.util.ArrayList;
