689d94bcb5ee3c2f7a900961534c22747bec0f52
Fatema <fatema.alibhayani@gmail.com>
2019-03-30 22:23:43 -0400

    Made a tradable interface

---
+++ phase2/phase2/Tradable.java
@@ -0,0 +1,46 @@
+package phase2;
+
+/**
+ * The interface Tradable, object that can be traded in the ATM.
+ */
+public interface Tradable{
+
+    /**
+     * Gets amount.
+     *
+     * @return the amount
+     */
+    double getAmount();
+
+    /**
+     * Add two tradable objects.
+     *
+     * @param t the tradable obejct
+     */
+    void add(Tradable t);
+
+    /**
+     * Subtract two tradable objects.
+     *
+     * @param t the tradable object
+     */
+    void subtract(Tradable t);
+
+    /**
+     * Multiply tradable.
+     *
+     * @param constant the constant to be multiplied
+     * @return the tradable object
+     */
+    Tradable multiply(double constant);
+
+    /**
+     * Convert tradable.
+     *
+     * @param identifier the 3 digit identifier of the tradable object
+     * @return the tradable
+     */
+    Tradable convert(String identifier);
+
+    String toString();
+}
