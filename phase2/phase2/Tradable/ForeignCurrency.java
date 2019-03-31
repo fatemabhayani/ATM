cf8809785ab6d799938b2d8f83d12e3e22ed2e0e
brittapal <brittany.palandra@mail.utoronto.ca>
2019-03-31 13:21:09 -0400

    merging

--- phase2/phase2/Tradable/ForeignCurrency.java
+++ phase2/phase2/Tradable/ForeignCurrency.java
@@ -57,7 +57,7 @@ public class ForeignCurrency implements Comparable<ForeignCurrency> {
     /**
      * Add two instances for foreign currencies.
      *
-     * @param f the second Tradable object
+     * @param f the second currency object
      */
     public void add(ForeignCurrency f){
         ForeignCurrency d =  f.convert(getCurrencyCode());
@@ -70,7 +70,7 @@ public class ForeignCurrency implements Comparable<ForeignCurrency> {
     /**
      * Subtract two instances for foreign currencies.
      *
-     * @param f the second Tradable object.
+     * @param f the second currency object.
      */
     public void subtract(ForeignCurrency f){
         ForeignCurrency d =  f.convert(getCurrencyCode());
