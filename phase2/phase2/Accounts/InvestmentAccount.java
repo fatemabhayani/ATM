321c5722bff24b4a67f69e9b8d3ed62ef4bfed4a
Sahni-Arjun <arjun.sahni@mail.utoronto.ca>
2019-03-31 18:50:50 -0400

    added a constructor to AssetAccount to easily insatntiate InvestmentAccount and added functionality to investment account

--- phase2/phase2/Accounts/InvestmentAccount.java
+++ phase2/phase2/Accounts/InvestmentAccount.java
@@ -1,4 +1,55 @@
 package phase2.Accounts;
 
-public class InvestmentAccount {
+import phase2.People.User;
+import phase2.Tradable.ForeignCurrency;
+import phase2.Tradable.Stock;
+import phase2.Transactions.Transaction;
+import phase2.Transactions.Withdraw;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Account for buying and selling stocks (bought and sold in USD by nature of the api used in Stock class)
+ */
+public class InvestmentAccount extends AssetAccount {
+
+
+    private ArrayList<Stock> stocks = new ArrayList<>();
+
+    public InvestmentAccount(Calendar date, User owner1, int num ){
+        super(date, owner1, num);
+        this.balance = new ForeignCurrency("USD", 0);
+    }
+
+
+    public void subtract(Transaction t){
+        if (t instanceof Withdraw ){
+            subtract(t.getAmount());
+        }
+    }
+
+    public void subtract(ForeignCurrency f){
+        if (balance.compareTo(f) >= 0){
+            balance.subtract(f);
+        } else {
+            System.out.println("You dont have enough money in your account to do this");
+        }
+    }
+
+    public double getStockPrice(String name){
+        Stock s = new Stock(name, 1);
+        return s.getPrice();
+    }
+
+    public void buyStock(String name, int volume){
+        Stock s = new Stock(name, volume);
+        if (balance.getAmount() > s.getTotalValue()){
+            balance.subtract(s.getTotalValue());
+            stocks.add(s);
+        }
+    }
+
+
+
 }
