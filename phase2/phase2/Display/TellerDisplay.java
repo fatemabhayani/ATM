2e06d89f18c745c653295c288517aae72ab5f708
brittapal <brittany.palandra@mail.utoronto.ca>
2019-03-31 21:08:30 -0400

    refactored tellerdisplay

--- phase2/phase2/Display/TellerDisplay.java
+++ phase2/phase2/Display/TellerDisplay.java
@@ -1,7 +1,6 @@
 package phase2.Display;
 
 import phase2.Accounts.Account;
-import phase2.Accounts.AccountManager;
 import phase2.People.*;
 import phase2.Request.Request;
 
@@ -11,167 +10,131 @@ import java.util.Scanner;
 public class TellerDisplay {
 
     /**
-     * The User.
-     */
-    public static final BankTeller b = (BankTeller) UserManager.getUser(ATM.username);
-    /**
-     * The Account.
-     */
-    public static Account a;
-    /**
-     * The accountType.
-     */
-    static String accountType;
-
-    /**
      * The entry point of User Display, access through ATM.
      *
      * @param args the input arguments
      */
     public static void main(String[] args) {
         ATMController controller = new ATMController();
+        BankTeller u = (BankTeller) UserManager.getUser(args[0]);
+        String[] args1 = new String[3];
+        args1[0] = args[0];
 
         Scanner sc = new Scanner(System.in);
         String command;
 
-        System.out.println("Type '0' to view requests. Type '1' to view Cash Machine alerts." +
-                "Type '2' to change password. Type'3' to interact with your accounts." +
-                " Type '4' to merge accounts with another user");
-        command = sc.nextLine();
-        command = command.replaceAll("//s", "");
-        while (!(command.toLowerCase().equals("0") || command.toLowerCase().equals("1")
-                || command.toLowerCase().equals("2")|| command.toLowerCase().equals("3")
-                || command.toLowerCase().equals("4"))) {
-            System.out.println("Wrong answer, try again");
-            System.out.println("Type '0' to view requests. Type '1' to view Cash Machine alerts." +
-                    "Type '2' to change password. Type'3' to interact with your accounts." +
-                    " Type '4' to merge accounts with another user");
-            command = sc.nextLine();
-            command = command.replaceAll("//s", "");
+        System.out.println("Type '0' if you would like to change your password." + "\n"
+                + "Type '1' if you would like to interact with your accounts." + "\n"
+                + "Type '2' if you would like to log out." + "\n" +
+                " Type '3' to merge accounts with another user." + "\n" +
+                " Type '4' to restock the cash machine." + "\n" +
+                " Type '5' to view your requests.");
+        Scanner tmp = new Scanner(System.in);
+        command = tmp.nextLine().replaceAll("//s","");
+        while (!(command.equals("0") || command.equals("1") || command.equals("2") || command.equals("3") ||
+                command.equals("4") || command.equals("5"))) {
+            System.out.println("You have not selected a valid action. Try again.");
+            command = tmp.nextLine().replaceAll("//s","");
         }
-        System.out.println(b.getRequestSummary());
-        if (b.getRequestSummary().equals("You have no requests.")) {
-            ATM.main(null);
-        }
+        switch (command) {
+            case "0":
+                System.out.println("Enter your new password:");
+                command = tmp.nextLine().replaceAll("//s", "");
+                u.setPassword(command);
+                System.out.println("Your password has been changed.");
+                UserDisplay.main(args);
+                break;
+            case "1":
+                System.out.println("This is the summary of your account balances.");
+                System.out.println(u.balanceSummary());
+                System.out.println("Please select the type of account you want to access.");
+                System.out.println("Type 'lc' for line of credit account." + "\n" + "Type 'cc' for credit card account." + "\n"
+                        + "Type 'cq' for chequing account." + "\n" + "Type 'sv' for savings account." + "\n" +
+                        "Type 'cb' for cash back card account.");
+                command = tmp.nextLine().toLowerCase();
+                while (!(command.equals("lc") || command.equals("cc") || command.equals("cb") || command.equals("cq") ||
+                        command.equals("sv"))) {
+                    System.out.println("You have not selected a valid account type. Try again.");
+                    command = tmp.nextLine().toLowerCase();
+                }
+                String accountType = command;
+                args1[1] = accountType;
+                ArrayList<Account> account = u.getAccountList(accountType);
 
-        boolean exited = false;
-        while (!exited) {
-            if (command.toLowerCase().equals("0")) {
-                System.out.println("Which request do you want to address? Put in the appropriate number only." + "\n" +
-                        "Type '0' for the oldest request");
-                command = sc.nextLine();
-                command = command.replaceAll("//s", "");
-                int number = Integer.valueOf(command);
-                while (number > b.getNumberOfRequests()) {
-                    System.out.println("This index is not assigned to a request. Please Try again");
-                    command = sc.nextLine();
-                    command = command.replaceAll("//s", "");
-                    number = Integer.valueOf(command);
-                }
-                Request request = b.getRequest(number);
-                System.out.println(request.toString());
-                System.out.println("Do you want to accept this request");
-                command = sc.nextLine();
-                command = command.replaceAll("//s", "");
-                controller.incorrectAns(sc, command);
-                if (command.equals("yes")) {
-                    b.completeRequest(number);
-                } else {
-                    b.ignoreRequest(number);
-                }
-                System.out.println("Would you like to exit?");
-                command = sc.nextLine();
-                command = command.replaceAll("//s", "");
-                if (command.toLowerCase().equals("yes")) {
-                    exited = true;
-                }
-            } else if (command.toLowerCase().equals("1")) {
-                b.restockCashMachine();
-                ATM.main(null);
-            } else if (command.toLowerCase().equals("2")) {
-                System.out.println("Enter new password:");
-                command = sc.nextLine();
-                command = command.replaceAll("//s", "");
-                b.setPassword(command);
-                UserDisplay.main(null);
-            } else if (command.toLowerCase().equals("3")) {
-                System.out.println(b.balanceSummary());
-                ArrayList<Account> account = new ArrayList<>();
-                System.out.println("Please select the type of account you want to access");
-                System.out.println("Type 'lc' for line of credit account" + "\n" + "Type 'cc' for credit card" + "\n"
-                        + "Type 'c' for chequing" + "\n" + "Type 's' for savings");
-                command = sc.nextLine();
-                switch (command) {
-                    case ("lc"):
-                        account = b.getAccountList("lc");
-                        break;
-                    case ("cc"):
-                        account = b.getAccountList("cc");
-                        break;
-                    case ("sv"):
-                        account = b.getAccountList("sv");
-                        break;
-                    case ("cq"):
-                        account = b.getAccountList("cq");
-                        break;
-                    case ("cb"):
-                        account = b.getAccountList("cb");
-                        break;
-                }
-                accountType = command;
-                a = null;
                 if (account.size() == 0) {
                     System.out.println("There are no accounts to choose from here! Do you want to request an account?");
-                    command = sc.nextLine();
-                    command = command.replaceAll("//s", "");
-                    controller.incorrectAns(sc, command);
+                    command = tmp.nextLine().replaceAll("//s", "").toLowerCase();
+                    controller.incorrectAns(tmp, command);
                     if (command.equals("yes")) {
-                        System.out.println("Enter the three digit currency code of the currency you want");
-                        command = sc.nextLine();
-                        command = command.replaceAll("//s", "");
-                        b.requestAccount(accountType, command);
+                        System.out.println("Enter the three digit currency code of the currency you want. Please " +
+                                "enter a valid currency code.");
+                        command = tmp.nextLine().replaceAll("//s", "");
+                        u.requestAccount(accountType, command);
                         System.out.println("Your request has been made!");
-                    } else {
-                        UserDisplay.main(null);
-                    }
-                    UserDisplay.main(null);
-                } else if (account.size() == 1) {
-                    a = account.get(0);
-                    AccountDisplay.main(null);
+                    } else { UserDisplay.main(args); }
+                    UserDisplay.main(args);
+
                 } else {
+                    System.out.println(u.accountSummary(accountType));
                     System.out.println("Enter the number of the account you wish to access: ");
-                    command = sc.nextLine();
-                    int number = Integer.valueOf(command.replaceAll("//s", ""));
-                    while (number > account.size()) {
-                        System.out.println("There is no account relative to this index. " +
-                                "Please input an appropriate index");
-                        command = sc.nextLine();
-                        number = Integer.valueOf(command.replaceAll("//s", ""));
+                    int number = Integer.valueOf(tmp.nextLine().replaceAll("//s",""));
+                    while (u.hasAccount(number)) {
+                        System.out.println("You do not have an account with this number. Try again.");
+                        number = Integer.valueOf(tmp.nextLine().replaceAll("//s",""));
                     }
-                    a = account.get(number);
-                    AccountDisplay.main(null);
+                    args1[2] = String.valueOf(number);
+                    AccountDisplay.main(args1);
                 }
-            } else if (command.toLowerCase().equals("4")) {
-                System.out.println("Type the username of the User you wish to join accounts with");
-                command = sc.nextLine();
+                break;
+            case "2":
+                ATM.main(null);
+                break;
+            case "3":
+                System.out.println("Type the username of the user you wish to join accounts with.");
+                command = tmp.nextLine();
                 User merger = UserManager.getUser(command);
                 while (merger == null) {
-                    System.out.println("There is no user with such a username. Please try again");
-                    command = sc.nextLine();
+                    System.out.println("There is no user with such a username. Please try again.");
+                    command = tmp.nextLine();
                     merger = UserManager.getUser(command);
                 }
-                System.out.println("Input the account number for the account you wish to share");
-                int number = Integer.valueOf(sc.nextLine().replaceAll("//s", ""));
-                AccountManager AM = new AccountManager(b);
-                Account a = AM.getAccount(number);
-                while (a == null) {
-                    System.out.print("There is no account relative to this index, Please select a valid account number");
-                    number = Integer.valueOf(sc.nextLine().replaceAll("//s", ""));
-                    a = AM.getAccount(number);
+                System.out.println("Input the account number for the account you wish to share.");
+                int number = Integer.valueOf(tmp.nextLine().replaceAll("//s",""));
+                while (u.hasAccount(number)) {
+                    System.out.println("You do not have an account with this number. Try again.");
+                    number = Integer.valueOf(tmp.nextLine().replaceAll("//s",""));
                 }
-                b.addAccountOwner(command, number);
-                ATM.main(null);
-            }
+                u.addAccountOwner(command, number);
+                break;
+            case("4"):
+                u.restockCashMachine();
+                TellerDisplay.main(args);
+                break;
+            case("5"):
+                System.out.println(u.getRequestSummary());
+                if (u.getRequestSummary().equals("You have no requests.")) {
+                    ManagerDisplay.main(null);
+                }
+                System.out.println("Which request do you want to address? Put in the appropriate number only." + "\n" +
+                        "Type '0' for the oldest request.");
+                command = sc.nextLine().replaceAll("//s", "");
+                int i = Integer.valueOf(command);
+                while (i > u.getNumberOfRequests()){
+                    System.out.println("This index is not assigned to a request. Please try again.");
+                    command = sc.nextLine().replaceAll("//s", "");
+                    i = Integer.valueOf(command);
+                }
+                Request request = u.getRequest(i);
+                System.out.println(request.toString());
+                System.out.println("Do you want to accept this request?");
+                command = sc.nextLine().replaceAll("//s", "");
+                controller.incorrectAns(sc,command);
+                if (command.toLowerCase().equals("yes")) {
+                    u.completeRequest(i);
+                } else {
+                    u.ignoreRequest(i);
+                }
+                TellerDisplay.main(args);
         }
     }
 }
