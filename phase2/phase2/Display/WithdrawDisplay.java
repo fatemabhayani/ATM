0dbd01e5a44f19bf3b3c16942e3dc77c3bb33a19
brittapal <brittany.palandra@mail.utoronto.ca>
2019-03-31 20:39:06 -0400

    refactored transfer and withdraw display

--- phase2/phase2/Display/WithdrawDisplay.java
+++ phase2/phase2/Display/WithdrawDisplay.java
@@ -2,7 +2,9 @@ package phase2.Display;
 
 import phase2.ATMTime;
 import phase2.Accounts.Account;
+import phase2.People.UserManager;
 import phase2.Tradable.ForeignCurrency;
+import phase2.Transactions.Bill;
 import phase2.Transactions.Withdraw;
 import phase2.People.User;
 
@@ -14,24 +16,23 @@ import java.util.Scanner;
  */
 class WithdrawDisplay {
 
-    private static final User U = AccountDisplay.U;
-    private static final Account a = AccountDisplay.a;
-
     /**
      * The entry point of Withdraw Display, access through ATM
      *
      * @param args the input arguments
      */
     public static void main(String[] args) {
+        User u = UserManager.getUser(args[0]);
+        Account a = UserManager.getUserAccount(Integer.valueOf(args[2]));
+
         Scanner tmp = new Scanner(System.in);
-        System.out.println("Enter the sum of money you wish to withdraw.");
+        System.out.println("Enter the sum of money you wish to withdraw:");
         String command = tmp.nextLine();
         int amt = Integer.valueOf(command.replaceAll("//s", ""));
-        Calendar time = ATMTime.getInstance().getCurrentTime();
-        Withdraw t = new Withdraw(new ForeignCurrency("CAD", amt), a, time);
-        U.makeTransaction(t);
+        Withdraw t = new Withdraw(new ForeignCurrency("CAD", amt), a, ATMTime.getInstance().getCurrentTime());
+        u.makeTransaction(t);
 
-        AccountDisplay.main(null);
+        AccountDisplay.main(args);
     }
 
 }
