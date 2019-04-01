807ab1e7a00c683b0c5092b2ca1f6ecafea01a82
Sahni-Arjun <arjun.sahni@mail.utoronto.ca>
2019-03-31 20:24:14 -0400

    changed design for stock and portfolio

--- phase2/phase2/Portfolio.java
+++ phase2/phase2/Portfolio.java
@@ -1,29 +1,68 @@
 package phase2;
 
+import phase2.Tradable.Stock;
+
 import java.util.HashMap;
 
 public class Portfolio {
 
     public static void main(String[] args) {
-        Portfolio p = new Portfolio();
-        p.addStock("AAPL", 100);
-        System.out.println(p.stockTable);
+
     }
-    private HashMap<String, Integer> stockTable;
+    private HashMap<String, Stock> stockTable;
+    private HashMap<String, Stock> shortTable;
 
     Portfolio(){
+
         this.stockTable = new HashMap<>();
+        this.shortTable = new HashMap<>();
     }
 
-    public void addStock(String name, int amount){
-        if (stockTable.containsKey(name)){
-            Integer num = stockTable.get(name);
-            num += amount;
-            stockTable.replace(name, num);
+    public void addStock(Stock s){
+        if (stockTable.containsKey(s.getName())){
+            Stock init = stockTable.get(s.getName());
+            init.add(s.getAmount());
         } else {
-            stockTable.put(name, amount);
+            stockTable.put(s.getName(), s);
         }
 
+    }
+
+    public double getValue(String name){
+        if (stockTable.containsKey(name)){
+            return stockTable.get(name).getTotalValue();
+        } else {
+            return 0;
+        }
+    }
+
+    public double getTotalValue(){
+        double amount = 0;
+        for(Stock s: stockTable.values()){
+            amount += s.getTotalValue();
+        }
+        return amount;
+    }
+
+    public void startShort(Stock s){
+        s.setShort();
+        if (!shortTable.containsKey(s.getName())){
+            shortTable.put(s.getName(), s);
+        } else {
+            System.out.println("You already have a short open with this stock, you are not allowed more");
+        }
+
+    }
+
+    public double endShort(String name){
+        if (shortTable.containsKey(name)){
+            Stock s = shortTable.get(name);
+            shortTable.remove(name);
+            return (s.getInitPrice() - s.getPrice()) * s.getAmount();
+        } else {
+            System.out.println("You did not have an open short with this name");
+            return 0;
+        }
     }
 
 
